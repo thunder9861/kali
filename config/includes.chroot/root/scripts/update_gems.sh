01f5bc3bed18870f41157dff5474c0b289b84f82
root <root@localhost.localdomain>
2013-06-19 02:04:31 +0000

    Updates

--- config/includes.chroot/root/scripts/update_gems.sh
+++ config/includes.chroot/root/scripts/update_gems.sh
@@ -3,12 +3,7 @@
 # Updates all gems
 # No arguments required
 
-gem1.9.2 update -y --no-rdoc --no-ri
-gem1.9.2 cleanup
-gem1.8 update -y --no-rdoc --no-ri
-gem1.8 cleanup
-rm -rf ~/ruby/1.9.2/doc/*
-rm -rf /usr/lib/ruby/gems/1.8/doc/*
-rm -rf /usr/lib/ruby/gems/1.9.2/doc/*
+gem update -y --no-rdoc --no-ri
+gem cleanup
 sync
 
