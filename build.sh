e3cadbb7795a4f42ce30e46176905b7e0d97f1ff
Joe Ruether <jrruethe@gmail.com>
2013-10-07 11:11:52 +0000

    Update scripts, trim some fat

--- build.sh
+++ build.sh
@@ -12,6 +12,7 @@ TARGET_DIR=$(dirname $0)/images/kali-$KALI_VERSION
 KALI_ARCH="i386"
 IMAGE_NAME="binary.hybrid.iso"
 KALI_CONFIG_OPTS="-- --proposed-updates"
+KERNEL=`apt-cache search linux-image | grep linux-image-$(uname -r) | grep -v dbg | awk '{print $1}' | sed 's/-686-pae//'`
 
 # Set sane PATH (cron seems to lack /sbin/ dirs)
 export PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
@@ -29,13 +30,11 @@ mkdir -p $TARGET_DIR
 # Build
 rm -vfr chroot/binary
 lb clean --purge >prepare.log 2>&1
-lb config -a $KALI_ARCH $KALI_CONFIG_OPTS >>prepare.log 2>&1
+lb config -a $KALI_ARCH --linux-packages $KERNEL $KALI_CONFIG_OPTS >>prepare.log 2>&1
 lb build
 
 if [ $? -ne 0 ] || [ ! -e $IMAGE_NAME ]; then
 	echo "Build of $KALI_ARCH live image failed" >&2
-	echo "Last 50 lines of the log:" >&2
-	tail -n 50 binary.log >&2
 	exit 1
 fi
 
--- clean.sh
+++ clean.sh
@@ -1,1 +1,4 @@
 git clean -d -f ./config/includes.chroot/
+lb clean noauto
+rm -rf config/binary config/bootstrap config/chroot config/common config/source
+rm -f binary.log prepare.log
