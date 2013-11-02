702e88738e86dbf87c7a4b0e73e5f56445ef8342
Joe Ruether <jrruethe@gmail.com>
2013-11-02 11:53:09 +0000

    Added local mirrors to speed up build process

--- build.sh
+++ build.sh
@@ -4,7 +4,16 @@ set -e
 
 # Speed up builds
 /etc/init.d/apt-cacher-ng start
-export http_proxy=http://localhost:3142/
+export http_proxy=http://127.0.0.1:3142/
+
+# Set mirror options
+KALI_MIRROR_OPRIONS=\
+'--mirror-bootstrap http://127.0.0.1:3142/archive.kali.org/kali '\
+'--mirror-debian-installer http://127.0.0.1:3142/archive.kali.org/kali '\
+'--mirror-chroot http://127.0.0.1:3142/archive.kali.org/kali '\
+'--mirror-chroot-security http://127.0.0.1:3142/archive.kali.org/kali-security '\
+'--mirror-binary http://127.0.0.1:3142/http.kali.org/kali '\
+'--mirror-binary-security http://127.0.0.1:3142/security.kali.org/kali-security '
 
 # Set variables
 KALI_VERSION="${VERSION:-daily}"
@@ -29,7 +38,7 @@ mkdir -p $TARGET_DIR
 
 # Build
 lb clean --purge >prepare.log 2>&1
-lb config -a $KALI_ARCH --linux-packages $KERNEL $KALI_CONFIG_OPTS --mirror-bootstrap "http://127.0.0.1/ftp.us.debian.org/debian/" >>prepare.log 2>&1
+lb config -a $KALI_ARCH --linux-packages $KERNEL $KALI_CONFIG_OPTS $KALI_MIRROR_OPTIONS >> prepare.log 2>&1
 lb build
 
 if [ $? -ne 0 ] || [ ! -e $IMAGE_NAME ]; then
