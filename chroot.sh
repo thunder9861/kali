b66c0b1cdf89054dcdec99cf9d9c2ca0dac4d7e0
Joe Ruether <jrruethe@gmail.com>
2013-11-03 22:45:34 +0000

    Added rungetty

---
+++ chroot.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+bin=./config/includes.chroot/root/bin/
+source=./chroot/
+destination=./config/includes.chroot/
+
+# Create an aufs layer
+$bin/aufs $source $destination
+
+# Chroot into the system
+$bin/mchroot $source
+
+# Unmount the aufs layer
+umount -v $source || umount -v -lf $source
+
+# Cleanup
+rm -rfv $destination/.wh..wh.aufs
+rm -rfv $destination/.wh..wh.orph
+rm -rfv $destination/.wh..wh.plnk
